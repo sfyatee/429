d7992a3c781fc6d1c15045ce581c81128166ca3e
demian garcia <dag@demians.org>
2026-04-01 21:16:03 -0700

    initial commit

---
+++ chat.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	// "net"
+	"fmt"
+	"os"
+
+	"chat/client"
+	"chat/srv"
+)
+
+func usage() {
+	fmt.Print("")
+	os.Exit(2)
+}
+
+func main() {
+	// help
+	// myip
+	// myport
+	// connect
+	// list
+	// terminate
+	// send
+	// exit
+	for {
+
+	}
+}
---
+++ srv/srv.go
@@ -0,0 +1,10 @@
+package srv
+
+import (
+	"log"
+	"net"
+)
+
+func Start() {
+
+}
